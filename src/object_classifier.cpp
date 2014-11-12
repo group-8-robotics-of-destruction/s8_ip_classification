905a7ab0fd2ad4f4665e1ac50d2f39b1a52aab3b
Unnar3 <unnar@unnar-Satellite-L650.(none)>
2014-11-12 17:25:00 +0100

    can now detect spheres and cubes as well as basic color detection

--- src/object_classifier.cpp
+++ src/object_classifier.cpp
@@ -13,8 +13,19 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+// OPENCV specific includes
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <message_filters/synchronizer.h>
+
 // OTHER
 #include <vector>
+#include <signal.h>
+#include <iostream>
 
 
 // DEFINITIONS
@@ -23,10 +34,15 @@
 
 #define NODE_NAME           		"s8_object_classification_node"
 #define TOPIC_POINT_CLOUD   		"/s8/detectedObject"
+#define TOPIC_RGB_IMAGE             "/camera/rgb/image_rect_color"
 #define TOPIC_EXTRACTED_OBJECTS		"/s8/modifiedObject"
+
+
+static const std::string OPENCV_WINDOW = "Image window";
 
 typedef pcl::PointXYZRGB PointT;
 using namespace std;
+using namespace cv;
 
 class ObjectClassifier : public s8::Node
 {
@@ -34,9 +50,12 @@ class ObjectClassifier : public s8::Node
 
     ros::Subscriber point_cloud_subscriber;
     ros::Publisher point_cloud_publisher;
+    ros::Subscriber rgb_image_subscriber;
     pcl::PointCloud<PointT>::Ptr cloud;
+    cv_bridge::CvImagePtr rgb_image;
 
     bool cloudInitialized;
+    bool rgbImageInitialized;
 
 public:
     ObjectClassifier(int hz) : hz(hz)
@@ -45,31 +64,74 @@ public:
         //printParams();
         point_cloud_subscriber = nh.subscribe(TOPIC_POINT_CLOUD, BUFFER_SIZE, &ObjectClassifier::point_cloud_callback, this);
         point_cloud_publisher  = nh.advertise<sensor_msgs::PointCloud2> (TOPIC_EXTRACTED_OBJECTS, BUFFER_SIZE);
-
+        rgb_image_subscriber   = nh.subscribe(TOPIC_RGB_IMAGE, BUFFER_SIZE, &ObjectClassifier::rgb_image_callback, this);
         cloud = pcl::PointCloud<PointT>::Ptr (new pcl::PointCloud<PointT>);
-        cloudInitialized = false;
+        cloudInitialized    = false;
+        rgbImageInitialized = false;
+
+        cv::namedWindow(OPENCV_WINDOW);
+    }
+    ~ObjectClassifier()
+    {
+        cv::destroyWindow(OPENCV_WINDOW);
     }
 
     void updateClass()
     {
         if ( cloudInitialized == false)
+        {
+            //ROS_INFO("No OBJECT FOUND");
             return;
-        getColors(cloud);
+        }
+        if(rgbImageInitialized == false) return;
 
+        bool isItCircle = isCircle();
+
+        if (isCircle())
+        {
+            findCircleClass();
+        }
+        else if (recognizePlaneObject(cloud))
+        {
+            ROS_INFO("I see a cube");
+        }
+        else
+            ROS_INFO("I don't see anything");
+        //else
+            //What is the color
+        //getColors(cloud);
+/*
+        std::vector<std::vector<float> > coefficientsPlane;
+        int inlierSizePlane = 0;
+        recognizePlaneObject(cloud, &coefficientsPlane, &inlierSizePlane);
+        float angle;
+        if (coefficientsPlane.size() > 1)
+        {
+            angle = getAngle(coefficientsPlane[0],coefficientsPlane[1]);
+            ROS_INFO("number of points: %d, planes: %d, angle: %f", inlierSizePlane, coefficientsPlane.size(), angle);
+        }
+        ROS_INFO("number of points: %d, planes: %d", inlierSizePlane, coefficientsPlane.size());
+*/
         cloudPublish(cloud);
         cloudInitialized = false;
     }
 
 private:
 
-    void getColors(pcl::PointCloud<PointT>::Ptr cloud_color)
+    void findCircleClass()
     {
-        uint8_t R_low = 255;
-        uint8_t R_high = 0;
-        uint8_t G_low = 255;
-        uint8_t G_high = 0;
-        uint8_t B_low = 255;
-        uint8_t B_high = 0;
+        float H = 0.0, S = 0.0, V = 0.0;
+        getColors(cloud, &H, &S, &V);
+        if(H > 0.9 || H < 0.1)
+            ROS_INFO("Seeing red circle");
+        else if (H < 0.7 && H > 0.5)
+            ROS_INFO("Seeing yellow circle");
+        else
+            ROS_INFO("Seeing something else");
+    }
+
+    void getColors(pcl::PointCloud<PointT>::Ptr cloud_color, float *H, float *S, float *V)
+    {
         double R_avg = 0;
         double G_avg = 0;
         double B_avg = 0;
@@ -79,9 +141,6 @@ private:
         float H_acc = 0;
         float S_acc = 0;
         float V_acc = 0;
-        float H_avg = 0;
-        float S_avg = 0;
-        float V_avg = 0;
         for(int iter = 0; iter != cloud_color->points.size(); ++iter)
         {
             //Whatever you want to do with the points
@@ -92,35 +151,18 @@ private:
             R_acc += R;
             G_acc += G;
             B_acc += B;
-            /*float H, S, V;
-            RGB2HSV((float)R, (float)G, (float)B, H, S, V);
-            H_acc += H;
-            S_acc += S;
-            V_acc += V;*/
-            //ROS_INFO("H: %f, S: %f, V: %f", H, S, V);
-/*            if (R_low > R)
-                R_low = R;
-            if (R_high < R)
-                R_high = R;
-            if (G_low > G)
-                G_low = G;
-            if (G_high < G)
-                G_high = G;
-            if (B_low > B)
-                B_low = B;
-            if (B_high < B)
-                B_high = B;*/
         }
         //ROS_INFO("R_low: %d R_high: %d G_low: %d G_high: %d B_low: %d B_high: %d", R_low, R_high, G_low, R_high, B_low, B_high);
         int size = cloud_color->points.size();
         R_avg = R_acc / size;
         G_avg = G_acc / size;
         B_avg = B_acc / size;
-        RGB2HSV((float)R_avg, (float)G_avg, (float)B_avg, H_avg, S_avg, V_avg);
+        RGB2HSV((float)R_avg, (float)G_avg, (float)B_avg, *H, *S, *V);
+        /*
         if (H_avg > 0.2 && H_avg < 0.4)
-            ROS_INFO("GREEN");
+            ROS_INFO("GREEN CUBE");
         else if (H_avg > 0.4 && H_avg < 0.7)
-            ROS_INFO("BLUE");
+            ROS_INFO("BLUE CUBE");
         else if (H_avg < 0.2 || H_avg > 0.7){
             if (H_avg < 0.18 && S_avg > 0.5 && V_avg > 0.4){
                 ROS_INFO("ORANGE STAR");}
@@ -130,7 +172,7 @@ private:
             ROS_INFO("RED");}
         else
             ROS_INFO("OTHER COLOR");
-        ROS_INFO("H_avg: %f, S_avg: %f, V_avg: %f", H_avg, S_avg, V_avg);
+        //ROS_INFO("H_avg: %f, S_avg: %f, V_avg: %f", H_avg, S_avg, V_avg);
         /*
         if (R_avg > G_avg && R_avg > B_avg)
             //ROS_INFO("RED");
@@ -139,6 +181,126 @@ private:
         else if (B_avg > G_avg && B_avg > R_avg)
             //ROS_INFO("BLUE");*/
         //ROS_INFO("R_avg: %lf G_avg: %lf B_avg: %lf R_acc: %lf G_acc: %lf B_acc: %lf cloud_size: %d", R_avg, G_avg, B_avg, R_acc, G_acc, B_acc, size);
+    }
+
+    bool recognizePlaneObject(pcl::PointCloud<PointT>::Ptr cloud_seg)
+    {
+        std::vector<std::vector<float> > coeffMatrix;
+        int inlierSize = 0;
+
+        pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT>);
+        pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+        pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+        pcl::ModelCoefficients coeff;
+        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+        pcl::ExtractIndices<PointT> extract;
+        pcl::NormalEstimation<PointT, pcl::Normal> ne;
+        pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg; 
+
+        // Estimate point normals
+        ne.setSearchMethod (tree);
+        ne.setKSearch (50);
+
+        seg.setOptimizeCoefficients (true);
+        seg.setModelType (pcl::SACMODEL_PLANE);
+        seg.setMethodType (pcl::SAC_RANSAC);
+        seg.setDistanceThreshold (0.001);
+        seg.setNormalDistanceWeight (0.1);
+        seg.setMaxIterations (1000);
+
+        int i = 0, nr_points = (int) cloud_seg->points.size ();
+        if (nr_points < 50){
+            return false;
+        }
+        // While 10% of the original cloud is still there
+        while (cloud_seg->points.size () > 0.1 * nr_points && i < 3)
+        {
+            //seg.setInputCloud (cloud);
+            ne.setInputCloud (cloud_seg);
+            ne.compute (*cloud_normals);
+            seg.setInputCloud (cloud_seg);
+            seg.setInputNormals (cloud_normals);
+            seg.segment (*inliers, coeff);
+            if (inliers->indices.size () == 0)
+            {
+                std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+                break;
+            }
+            if(inliers->indices.size() < nr_points*0.05|| inliers->indices.size() < 20){
+                i++;
+                continue;
+            }
+            // Extract the planar inliers from the input cloud
+            extract.setInputCloud (cloud_seg);
+            extract.setIndices (inliers);
+            extract.setNegative (true);
+            extract.filter (*cloud_plane);
+            cloud_seg.swap (cloud_plane);
+            coeffMatrix.push_back(coeff.values);
+            i++;
+            if (inliers->indices.size() > inlierSize)
+                inlierSize = inliers->indices.size();
+        }
+        if (inlierSize > 50)
+        {
+            if (coeffMatrix.size() == 2)
+            {
+                float angle = getAngle(coeffMatrix[0],coeffMatrix[1]);
+                if (abs(angle - 90) > 8 )
+                    return false;
+            }
+            else if (coeffMatrix.size() == 3)
+            {
+                float angle1 = getAngle(coeffMatrix[0],coeffMatrix[1]);
+                float angle2 = getAngle(coeffMatrix[0],coeffMatrix[2]);
+                float angle3 = getAngle(coeffMatrix[1],coeffMatrix[2]);
+                if (abs(angle1 - 90) > 8 || abs(angle2 - 90) > 8 || abs(angle3 - 90) > 8 )
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool isCircle()
+    {
+        if(rgbImageInitialized == false) return false;
+
+        Mat HSVImage;
+        Mat RGBImage;
+
+        // Convert from BRG to HSV and use gaussian blur.
+        colorPreProcess(&HSVImage, &RGBImage);
+
+        Mat imgThresholded;
+
+        Mat cimg;
+        Mat maskedimg;
+        RGBImage.copyTo(maskedimg, imgThresholded);
+        medianBlur(maskedimg, maskedimg, 5);
+
+        cvtColor(maskedimg, cimg, COLOR_BGR2GRAY);
+
+        vector<Vec3f> circles;
+        HoughCircles(cimg, circles, CV_HOUGH_GRADIENT, 1, 10,
+                     100, 30, 1, 30 // change the last two parameters
+                                    // (min_radius & max_radius) to detect larger circles
+                     );
+        
+        for( size_t i = 0; i < circles.size(); i++ )
+        {
+            Vec3i c = circles[i];
+            circle( maskedimg, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, CV_AA);
+            circle( maskedimg, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, CV_AA);
+        }
+        cv::imshow(OPENCV_WINDOW, maskedimg);
+        cv::waitKey(3);
+        if (circles.size() > 0)
+            return true;
+        else
+            return false;
     }
 
     static void RGB2HSV(float r, float g, float b, float &h, float &s, float &v)
@@ -163,10 +325,44 @@ private:
         v = r/255;
     }
 
+    float getAngle (std::vector<float> v1, std::vector<float> v2)
+    {
+        float dotproduct    = v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
+        float len1          = v1[0]*v1[0]+v1[1]*v1[1]+v1[2]*v1[2];
+        float len2          = v2[0]*v2[0]+v2[1]*v2[1]+v2[2]*v2[2];
+        float angleRad      = acos(dotproduct/(sqrt(len1)*sqrt(len2)));
+        return angleRad*180/3.1415;
+    }
+
+    void colorPreProcess(cv::Mat *HSVImage, cv::Mat *RGBImage)
+    {
+        cv::Mat tempHSV, tempRGB;
+        //Convert the captured frame from BGR to HSV
+        cvtColor(rgb_image->image, tempHSV, COLOR_BGR2HSV);
+        tempRGB = rgb_image->image;
+        *HSVImage = tempHSV;
+        *RGBImage = tempRGB;
+        GaussianBlur( *RGBImage, *RGBImage, Size(5, 5), 1, 1 );
+    }
+
     void point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     {
         pcl::fromROSMsg (*cloud_msg, *cloud);
         cloudInitialized = true;
+    }
+
+    void rgb_image_callback(const sensor_msgs::ImageConstPtr& msgColor)
+    {
+        try
+        {
+            rgb_image = cv_bridge::toCvCopy(msgColor, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        rgbImageInitialized = true;
     }
 
     void cloudPublish(pcl::PointCloud<PointT>::Ptr cloud_pub)
